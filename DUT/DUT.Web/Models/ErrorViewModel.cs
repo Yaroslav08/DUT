cacbaa38ff3824e8000de659540b3fc295106708
Yaroslav Mudryk <yaroslav.mudryk@gmail.com>
2021-12-20 21:10:06 +0200

    Project init

---
+++ DUT/DUT.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DUT.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
