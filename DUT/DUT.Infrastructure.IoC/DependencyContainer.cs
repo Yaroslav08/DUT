18780701bf6a92226d7df43c49288e2d2464f732
Yaroslav Mudryk <yaroslav.mudryk@gmail.com>
2021-12-24 18:30:05 +0200

    Init full struct of project

---
+++ DUT/DUT.Infrastructure.IoC/DependencyContainer.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace DUT.Infrastructure.IoC
+{
+    public static class DependencyContainer
+    {
+        public static IServiceCollection AddDUTServices(this IServiceCollection services)
+        {
+
+
+
+            return services;
+        }
+    }
+}
