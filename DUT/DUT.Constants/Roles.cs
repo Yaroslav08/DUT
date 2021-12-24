cea7a3709ac2589ff3214daaeddb2c2599e26450
Yaroslav Mudryk <yaroslav.mudryk@gmail.com>
2021-12-24 18:11:58 +0200

    project with constants was added

---
+++ DUT/DUT.Constants/Roles.cs
@@ -0,0 +1,10 @@
+﻿namespace DUT.Constants
+{
+    public static class Roles
+    {
+        public const string Admin = "Administrator";
+        public const string Moderator = "Moderator";
+        public const string Teacher = "Teacher";
+        public const string Student = "Student";
+    }
+}
