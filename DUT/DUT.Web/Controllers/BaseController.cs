b4e4769bbc575b5279d0c04cd3435f6a6d9f3754
Yaroslav Mudryk <yaroslav.mudryk@gmail.com>
2021-12-27 14:21:59 +0200

    Added basic methods for receiving user data

--- DUT/DUT.Web/Controllers/BaseController.cs
+++ DUT/DUT.Web/Controllers/BaseController.cs
@@ -1,13 +1,66 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-
+using System.Security.Claims;
 namespace DUT.Web.Controllers
 {
     public class BaseController : Controller
     {
-        public IActionResult Index()
+        protected string[] GetUserRoles()
         {
-            var meta = HttpContext.GetEndpoint();
-            return View();
+            if (User.Identity.IsAuthenticated)
+                return User.Claims.Where(x => x.Type == ClaimTypes.Role).Select(x => x.Value).ToArray();
+            return null;
+        }
+
+        protected string GetFullName()
+        {
+            if (User.Identity.IsAuthenticated)
+                return GetValueFromClaimByType("FullName");
+            return null;
+        }
+
+        protected string GetFirstName()
+        {
+            if (User.Identity.IsAuthenticated)
+                return GetValueFromClaimByType("FirstName");
+            return null;
+        }
+
+        protected string GetLastName()
+        {
+            if (User.Identity.IsAuthenticated)
+                return GetValueFromClaimByType("LastName");
+            return null;
+        }
+
+        protected string GetUserName()
+        {
+            if (User.Identity.IsAuthenticated)
+                return GetValueFromClaimByType(ClaimTypes.Name);
+            return null;
+        }
+
+        protected int GetUserId()
+        {
+            if (User.Identity.IsAuthenticated)
+                return Convert.ToInt32(GetValueFromClaimByType(ClaimTypes.NameIdentifier));
+            return 0;
+        }
+
+        protected int GetSessionId()
+        {
+            if (User.Identity.IsAuthenticated)
+                return Convert.ToInt32(GetValueFromClaimByType("SessionId"));
+            return 0;
+        }
+
+        protected string GetUserAgent()
+        {
+            return HttpContext.Request.Headers["User-Agent"].ToString();
+        }
+
+        private string GetValueFromClaimByType(string type)
+        {
+            return User.Claims.FirstOrDefault(x => x.Type == type).Value;
         }
     }
 }
