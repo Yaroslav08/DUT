04e7af807c167737a7bd28e1bd9522b21ab91a40
Yaroslav Mudryk <yaroslav.mudryk@gmail.com>
2021-12-27 20:22:57 +0200

    Added new role Super Admin

--- DUT/DUT.Constants/Roles.cs
+++ DUT/DUT.Constants/Roles.cs
@@ -2,6 +2,7 @@
 {
     public static class Roles
     {
+        public const string SuperAdmin = "Super Administrator";
         public const string Admin = "Administrator";
         public const string Moderator = "Moderator";
         public const string Teacher = "Teacher";
