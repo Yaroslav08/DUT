12de2c43dd570e0a7accbb5f7ca84a8dbe299c5f
Yaroslav Mudryk <yaroslav.mudryk@gmail.com>
2021-12-27 16:46:15 +0200

    Added tests for constants project

---
+++ DUT/DUT.Constants.Tests/GeneratorTests.cs
@@ -0,0 +1,40 @@
+﻿using Xunit;
+namespace DUT.Constants.Tests
+{
+    public class GeneratorTests
+    {
+        [Fact]
+        public void GroupInviteCodeLength()
+        {
+            var code = Generator.CreateGroupInviteCode();
+            Assert.Equal(9, code.Length);
+        }
+
+        [Theory]
+        [InlineData(10000, 1)]
+        [InlineData(100000, 5)]
+        public void CheckOnUniqGroupInviteCode(int commonCount, int maxRepeat)
+        {
+            Dictionary<string, int> dictionaryOfInviteCodes = new Dictionary<string, int>();
+
+            string code = "";
+
+            for (int i = 0; i < commonCount; i++)
+            {
+                code = Generator.CreateGroupInviteCode();
+                if (dictionaryOfInviteCodes.ContainsKey(code))
+                {
+                    dictionaryOfInviteCodes[code]++;
+                }
+                else
+                {
+                    dictionaryOfInviteCodes.Add(code, 1);
+                }
+            }
+
+            var count = dictionaryOfInviteCodes.Where(x => x.Value > 1).Count();
+
+            Assert.True(count <= maxRepeat);
+        }
+    }
+}
