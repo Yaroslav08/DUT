da838afcea7f00f1c9129e8a2c1664fbb7a7712e
Yaroslav Mudryk <yaroslav.mudryk@gmail.com>
2021-12-27 13:37:12 +0200

    Added SessionConfiguration

---
+++ DUT/DUT.Infrastructure.Data/Configurations/SessionConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using DUT.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System.Text.Json;
+
+namespace DUT.Infrastructure.Data.Configurations
+{
+    public class SessionConfiguration : IEntityTypeConfiguration<Session>
+    {
+        public void Configure(EntityTypeBuilder<Session> builder)
+        {
+            builder.HasKey(x => x.Id);
+        }
+    }
+}
---
+++ DUT/DUT.Infrastructure.Data/Context/DUTContext.cs
@@ -0,0 +1,28 @@
+﻿using DUT.Domain.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace DUT.Infrastructure.Data.Context
+{
+    public class DUTContext : IdentityDbContext<User, Role, int, UserClaim, UserRole, UserLogin, RoleClaim, UserToken>
+    {
+        #region ctors
+        public DUTContext(DbContextOptions<DUTContext> options) : base(options)
+        {
+
+        }
+        public DUTContext()
+        {
+
+        }
+        #endregion
+
+        public DbSet<Session> Sessions { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+        }
+    }
+}
