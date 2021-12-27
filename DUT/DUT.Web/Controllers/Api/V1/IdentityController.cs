bfeb994acfbe233e556f6764e91ed1b5da0b433e
Yaroslav Mudryk <yaroslav.mudryk@gmail.com>
2021-12-27 19:03:08 +0200

    Added IdentityController

---
+++ DUT/DUT.Web/Controllers/Api/V1/IdentityController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+namespace DUT.Web.Controllers.Api.V1
+{
+    [ApiVersion("1.0")]
+    public class IdentityController : ApiBaseController
+    {
+
+        [HttpPost("login")]
+        public IActionResult Login()
+        {
+            return Ok();
+        }
+
+        [HttpPost("registration")]
+        public IActionResult Registration()
+        {
+            return Ok();
+        }
+
+        [HttpPost("logout")]
+        public IActionResult Logout()
+        {
+            return Ok();
+        }
+    }
+}
