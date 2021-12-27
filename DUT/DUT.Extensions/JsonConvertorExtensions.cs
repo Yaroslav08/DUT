fcaae8d7fc8d88d24b1247f976c33baabac39e2b
Yaroslav Mudryk <yaroslav.mudryk@gmail.com>
2021-12-27 13:40:40 +0200

    Add extension for work with json

---
+++ DUT/DUT.Extensions/JsonConvertorExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System.Text.Json;
+
+namespace DUT.Extensions
+{
+    public static class JsonConvertorExtensions
+    {
+        private static readonly JsonSerializerOptions _options = new JsonSerializerOptions
+        {
+            
+        };
+        public static string ToJson(this object data)
+        {
+            return JsonSerializer.Serialize(data, _options);
+        }
+        public static T FromJson<T>(this string json)
+        {
+            return JsonSerializer.Deserialize<T>(json, _options);
+        }
+    }
+}
