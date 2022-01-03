2cef736e7d2ef73238b723ec5b5129a62062d468
Yaroslav Mudryk <yaroslav.mudryk@gmail.com>
2022-01-03 17:51:06 +0200

    Added fullName mapper for viewModel

--- DUT/DUT.Application/ViewModels/Mapper.cs
+++ DUT/DUT.Application/ViewModels/Mapper.cs
@@ -1,5 +1,7 @@
 ﻿using AutoMapper;
 using DUT.Application.ViewModels.Group;
+using DUT.Application.ViewModels.User;
+using System.Text;
 
 namespace DUT.Application.ViewModels
 {
@@ -9,6 +11,24 @@ namespace DUT.Application.ViewModels
         {
             CreateMap<Domain.Models.Group, GroupViewModel>()
                 .ForMember(x => x.Name, s => s.MapFrom(x => $"{x.Name} ({x.StartStudy.Year})"));
+
+
+            CreateMap<Domain.Models.User, UserViewModel>()
+                .ForMember(x => x.FullName, s => s.MapFrom(s => BuildFullName(s)));
+        }
+
+        private string BuildFullName(Domain.Models.User user)
+        {
+            var sb = new StringBuilder();
+            sb.Append(user.LastName);
+            sb.Append(" ");
+            sb.Append(user.FirstName);
+            if (!string.IsNullOrEmpty(user.MiddleName) && !string.IsNullOrWhiteSpace(user.MiddleName))
+            {
+                sb.Append(" ");
+                sb.Append(user.MiddleName);
+            }
+            return sb.ToString();
         }
     }
 }
--- DUT/DUT.Application/ViewModels/User/UserViewModel.cs
+++ DUT/DUT.Application/ViewModels/User/UserViewModel.cs
@@ -3,11 +3,14 @@
     public class UserViewModel
     {
         public int Id { get; set; }
-        public DateTime JoinAt { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
         public string FullName { get; set; }
-        public string Username { get; set; }
+        public string FirstName { get; set; }
+        public string MiddleName { get; set; }
+        public string LastName { get; set; }
+        public string UserName { get; set; }
+        public DateTime JoinAt { get; set; }
         public string Image { get; set; }
+        public string ContactEmail { get; set; }
+        public string ContactPhone { get; set; }
     }
 }
