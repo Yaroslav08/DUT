0affe6c4ae3bab461c2cabdae2ff93adc8a0334a
Yaroslav Mudryk <yaroslav.mudryk@gmail.com>
2022-01-06 19:19:59 +0200

    Added methods for generators

--- DUT/DUT.Constants/Generator.cs
+++ DUT/DUT.Constants/Generator.cs
@@ -29,5 +29,58 @@ namespace DUT.Constants
 
             return sb.ToString();
         }
+
+        public static string CreateAppId()
+        {
+            return GetUniqCode(4);
+        }
+
+        public static string CreateAppSecret()
+        {
+            return GetString(70);
+        }
+
+        public static string GetUniqCode(int sections)
+        {
+            var commonWords = sections * 4;
+            var commonCountOfSymbols = commonWords + (sections - 1);
+            var stringChars = new char[commonCountOfSymbols];
+            var positons = GetHyphenPositions(sections);
+            var random = new Random();
+            for (int i = 0; i < stringChars.Length; i++)
+            {
+                if (positons.Contains(i))
+                {
+                    stringChars[i] = '-';
+                    continue;
+                }
+                stringChars[i] = _chars[random.Next(_chars.Length)];
+            }
+            return new String(stringChars);
+        }
+
+        private static int[] GetHyphenPositions(int sections)
+        {
+            var pos = new int[sections - 1];
+            var baseIndex = 4;
+            for (int i = 0; i < pos.Length; i++)
+            {
+                pos[i] = baseIndex;
+                baseIndex += 4 + 1;
+            }
+            return pos;
+        }
+
+        public static string GetString(int length, bool IsUpper = false, bool IsLowwer = false)
+        {
+            var stringChars = new char[length];
+            var random = new Random();
+            for (int i = 0; i < stringChars.Length; i++)
+            {
+                stringChars[i] = _chars[random.Next(_chars.Length)];
+            }
+            var result = new string(stringChars);
+            return IsUpper ? result.ToUpper() : IsLowwer ? result.ToLower() : result;
+        }
     }
 }
