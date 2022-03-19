6a4f4c20459c68430a29af81e31e27ea49deaa6a
Yaroslav Mudryk <yaroslav.mudryk@gmail.com>
2022-03-19 17:01:04 +0200

    Added endpoints

--- DUT/DUT.Web/Controllers/V1/TimetableController.cs
+++ DUT/DUT.Web/Controllers/V1/TimetableController.cs
@@ -1,4 +1,5 @@
 ﻿using DUT.Application.Services.Interfaces;
+using DUT.Application.ViewModels.Timetable;
 using Microsoft.AspNetCore.Mvc;
 
 namespace DUT.Web.Controllers.V1
@@ -14,7 +15,37 @@ namespace DUT.Web.Controllers.V1
             _timetableService = timetableService;
         }
 
+        [HttpGet]
+        public async Task<IActionResult> GetGroupTimetable(int groupId, DateTime? from, DateTime? to)
+        {
+            if (from == null)
+                from = DateTime.Today;
+            if (to == null)
+                to = DateTime.Today.AddMonths(1);
+            if ((from.HasValue && to.HasValue) && from > to)
+            {
+                from = DateTime.Today;
+                to = DateTime.Today.AddMonths(1);
+            }
+            return JsonResult(await _timetableService.GetTimetableBetweenDatesAsync(groupId, from.Value, to.Value));
+        }
 
+        [HttpPost]
+        public async Task<IActionResult> CreateTimetable([FromBody] TimetableCreateModel timetable)
+        {
+            return JsonResult(await _timetableService.CreateTimetableAsync(timetable));
+        }
 
+        [HttpPut]
+        public async Task<IActionResult> UpdateTimetable([FromBody] TimetableCreateModel timetable)
+        {
+            return JsonResult(await _timetableService.UpdateTimetableAsync(timetable));
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> DeleteTimetable(long[] ids)
+        {
+            return JsonResult(await _timetableService.RemoveTimetableAsync(ids));
+        }
     }
 }
