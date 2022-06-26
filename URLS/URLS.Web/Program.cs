56f05bbf1934a46ef9a535c6846096e8d4f42d6d
Mykyta Medko <nikita.jove28@gmail.com>
2022-06-26 14:12:19 +0300

    Fix top lvl program

--- URLS/URLS.Web/Program.cs
+++ URLS/URLS.Web/Program.cs
@@ -9,157 +9,163 @@ using URLS.Infrastructure.IoC;
 using URLS.Web.Filters;
 using URLS.Web.Middlewares;
 
-var builder = WebApplication.CreateBuilder(args);
-
-#region Services
-
-builder.Services.Configure<ApiBehaviorOptions>(options =>
+public class Program
 {
-    options.SuppressModelStateInvalidFilter = true;
-});
-
-builder.Services.AddApiVersioning(options =>
-{
-    options.AssumeDefaultVersionWhenUnspecified = true;
-    options.DefaultApiVersion = new ApiVersion(1, 0);
-    options.ReportApiVersions = true;
-});
-
-builder.Services.AddDirectoryBrowser();
-
-builder.Services.AddResponseCompression(options => options.EnableForHttps = true);
-
-builder.Services.AddEndpointsApiExplorer();
-
-builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-    .AddJwtBearer(jwt =>
+    public static async Task Main(string[] args)
     {
-        jwt.RequireHttpsMetadata = false;
-        jwt.SaveToken = true;
-        jwt.TokenValidationParameters = new TokenValidationParameters
+        var builder = WebApplication.CreateBuilder(args);
+
+        #region Services
+
+        builder.Services.Configure<ApiBehaviorOptions>(options =>
         {
-            ValidateIssuer = true,
-            ValidIssuer = TokenOptions.Issuer,
-            ValidateAudience = true,
-            ValidAudience = TokenOptions.Audience,
-            ValidateLifetime = true,
-            IssuerSigningKey = TokenOptions.GetSymmetricSecurityKey(),
-            ValidateIssuerSigningKey = true,
-        };
-    })
-    .AddGoogle(google =>
-    {
-        google.ClientId = builder.Configuration["Credentials:Google:Id"];
-        google.ClientSecret = builder.Configuration["Credentials:Google:Secret"];
-        google.SaveTokens = true;
-    });
+            options.SuppressModelStateInvalidFilter = true;
+        });
 
-builder.Services.AddLogging(x =>
-{
-    x.AddConsole();
-});
-
-builder.Services.AddURLSServices(builder.Configuration);
-
-builder.Services.AddControllers(options =>
-{
-    options.Filters.Add(new ModelStateValidatorAttribute());
-});
-
-builder.Services.AddSwaggerGen(s =>
-{
-    s.SwaggerDoc("v1", new OpenApiInfo
-    {
-        Version = "v1",
-        Title = "(Ukrainian Remote Learning System) API",
-    });
-
-
-    var jwtSecurityScheme = new OpenApiSecurityScheme
-    {
-        Scheme = "bearer",
-        BearerFormat = "JWT",
-        Name = "JWT Authentication",
-        In = ParameterLocation.Header,
-        Type = SecuritySchemeType.Http,
-        Description = "Put your JWT Bearer token on textbox below!",
-
-        Reference = new OpenApiReference
+        builder.Services.AddApiVersioning(options =>
         {
-            Id = JwtBearerDefaults.AuthenticationScheme,
-            Type = ReferenceType.SecurityScheme
-        }
-    };
+            options.AssumeDefaultVersionWhenUnspecified = true;
+            options.DefaultApiVersion = new ApiVersion(1, 0);
+            options.ReportApiVersions = true;
+        });
 
-    s.AddSecurityDefinition(jwtSecurityScheme.Reference.Id, jwtSecurityScheme);
+        builder.Services.AddDirectoryBrowser();
 
-    s.AddSecurityRequirement(new OpenApiSecurityRequirement
+        builder.Services.AddResponseCompression(options => options.EnableForHttps = true);
+
+        builder.Services.AddEndpointsApiExplorer();
+
+        builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+            .AddJwtBearer(jwt =>
+            {
+                jwt.RequireHttpsMetadata = false;
+                jwt.SaveToken = true;
+                jwt.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidIssuer = TokenOptions.Issuer,
+                    ValidateAudience = true,
+                    ValidAudience = TokenOptions.Audience,
+                    ValidateLifetime = true,
+                    IssuerSigningKey = TokenOptions.GetSymmetricSecurityKey(),
+                    ValidateIssuerSigningKey = true,
+                };
+            })
+            .AddGoogle(google =>
+            {
+                google.ClientId = builder.Configuration["Credentials:Google:Id"];
+                google.ClientSecret = builder.Configuration["Credentials:Google:Secret"];
+                google.SaveTokens = true;
+            });
+
+        builder.Services.AddLogging(x =>
+        {
+            x.AddConsole();
+        });
+
+        builder.Services.AddURLSServices(builder.Configuration);
+
+        builder.Services.AddControllers(options =>
+        {
+            options.Filters.Add(new ModelStateValidatorAttribute());
+        });
+
+        builder.Services.AddSwaggerGen(s =>
+        {
+            s.SwaggerDoc("v1", new OpenApiInfo
+            {
+                Version = "v1",
+                Title = "(Ukrainian Remote Learning System) API",
+            });
+
+
+            var jwtSecurityScheme = new OpenApiSecurityScheme
+            {
+                Scheme = "bearer",
+                BearerFormat = "JWT",
+                Name = "JWT Authentication",
+                In = ParameterLocation.Header,
+                Type = SecuritySchemeType.Http,
+                Description = "Put your JWT Bearer token on textbox below!",
+
+                Reference = new OpenApiReference
+                {
+                    Id = JwtBearerDefaults.AuthenticationScheme,
+                    Type = ReferenceType.SecurityScheme
+                }
+            };
+
+            s.AddSecurityDefinition(jwtSecurityScheme.Reference.Id, jwtSecurityScheme);
+
+            s.AddSecurityRequirement(new OpenApiSecurityRequirement
     {
         { jwtSecurityScheme, Array.Empty<string>() }
     });
-});
+        });
 
-#endregion
+        #endregion
 
-var app = builder.Build();
+        var app = builder.Build();
 
-#region Pipeline
+        #region Pipeline
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseMigrationsEndPoint();
-    app.UseSwagger();
-    app.UseSwaggerUI();
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseMigrationsEndPoint();
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+        else
+        {
+            app.UseExceptionHandler("/Home/Error");
+            app.UseHsts();
+        }
+
+        app.UseResponseCompression();
+
+        app.UseAccessTokenHandler();
+
+        app.UseGlobalErrorHandler();
+
+        app.UseApiVersioning();
+
+        app.UseHttpsRedirection();
+        app.UseStaticFiles();
+
+        app.UseFileServer(new FileServerOptions
+        {
+            FileProvider = new PhysicalFileProvider(
+                   Path.Combine(builder.Environment.ContentRootPath, "wwwroot/Images")),
+            RequestPath = "/files",
+            EnableDirectoryBrowsing = true
+        });
+
+        app.UseRouting();
+
+        app.UseSessionHandler();
+
+        app.UseAuthentication();
+        app.UseAuthorization();
+
+        app.UseStatistics();
+
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapControllers();
+        });
+
+        #endregion
+
+        #region DataSeeder
+
+        using (var scope = app.Services.CreateScope())
+        {
+            var seederService = scope.ServiceProvider.GetService<ISeederService>();
+            await seederService.SeedSystemAsync();
+        }
+
+        #endregion
+
+        app.Run();
+    }
 }
-else
-{
-    app.UseExceptionHandler("/Home/Error");
-    app.UseHsts();
-}
-
-app.UseResponseCompression();
-
-app.UseAccessTokenHandler();
-
-app.UseGlobalErrorHandler();
-
-app.UseApiVersioning();
-
-app.UseHttpsRedirection();
-app.UseStaticFiles();
-
-app.UseFileServer(new FileServerOptions
-{
-    FileProvider = new PhysicalFileProvider(
-           Path.Combine(builder.Environment.ContentRootPath, "wwwroot/Images")),
-    RequestPath = "/files",
-    EnableDirectoryBrowsing = true
-});
-
-app.UseRouting();
-
-app.UseSessionHandler();
-
-app.UseAuthentication();
-app.UseAuthorization();
-
-app.UseStatistics();
-
-app.UseEndpoints(endpoints =>
-{
-    endpoints.MapControllers();
-});
-
-#endregion
-
-#region DataSeeder
-
-using (var scope = app.Services.CreateScope())
-{
-    var seederService = scope.ServiceProvider.GetService<ISeederService>();
-    await seederService.SeedSystemAsync();
-}
-
-#endregion
-
-app.Run();
