e1074fcf88d4c737fa8f20e66096616c15cb2696
Yaroslav Mudryk <yaroslav.mudryk@gmail.com>
2022-08-05 12:52:44 +0300

    Update DependencyContainer.cs

--- URLS/URLS.Infrastructure.IoC/DependencyContainer.cs
+++ URLS/URLS.Infrastructure.IoC/DependencyContainer.cs
@@ -36,7 +36,7 @@ namespace URLS.Infrastructure.IoC
 
             #region Services
 
-            services.AddScoped<ISeederService, BaseSeederService>();
+            services.AddScoped<ISeederService, DUTSeederService>();
             services.AddScoped<IUniversityService, UniversityService>();
             services.AddScoped<IIdentityService, HttpIdentityService>();
             services.AddScoped<IFacultyService, FacultyService>();
